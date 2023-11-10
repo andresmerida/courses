626b8ee0fefa0a7e43635482a4f810ee731c755e
Andres <andresmerida1@gmail.com>
2023-11-10 19:11:42 -0400

    first commit

---
+++ src/main/java/com/diplomado/courses/CoursesApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomado/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomado.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
